c17438efa7dd763f563d168a6734cb5272faff5d
deepak252 <deepakkudc16102000@gmail.com>
2024-03-14 16:28:26 +0530

    Props validation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <!-- <Greet name="Bruce" heroName="Batman"/> -->
   <!-- NOTE: Prefer using kebab-case for props in HTML and camelCase for the same in JS -->
-  <Greet name="Bruce" hero-name="Batman"/>
-  <Greet name="Clark" hero-name="Superman"/> <!-- Passing static values to props -->
-  <Greet :name="name" :hero-name="name2"/> <!-- Passing dynamic values to props -->
+  <!-- <Greet name="Bruce" hero-name="Batman"/>
+  <Greet name="Clark" hero-name="Superman"/> 
+  <Greet :name="name" :hero-name="name2"/> -->
+  <Article :likes="8"/>
+  <Article title="Title 1" :likes="12"/>
+  <Article :title="'Title 2'" content="Lorem Ipsum" :likes="12"/>
+
 </template>
 
 <script>
-import Greet from'./components/Greet.vue'
+// import Greet from'./components/Greet.vue'
+import Article from'./components/Article.vue'
 
 export default {
   name: 'App',
   components: {
-    Greet
+    // Greet,
+    Article
   },
   data(){
     return {
---
+++ src/components/Article.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Title - {{title}}</h2>
+        <p>{{content}}</p>
+        <p>Likes - {{likes}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Article',
+        props: {
+            title: {
+                type: String,
+                default: "Article Default Title"
+            },
+            content: String,
+            likes: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
