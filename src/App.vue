306882ed19e60f7071c20f9544940164f38c3995
deepak252 <deepakkudc16102000@gmail.com>
2024-03-14 11:54:22 +0530

    Watchers example

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <h2>Total Amount: {{totalAmount}}</h2>
-  <h2>Total Amount: {{getTotalAmount()}}</h2>
-  <button @click="prices.push(5)">Add Item</button>
-  <input type="text" v-model="name"/>
-  <span v-for="amount in highAmounts" :key="amount">' '{{amount}}</span>
-  <p>Full Name: {{fullName}}</p>
-  <button @click="changeFullName">Change Full Name</button>
+  <h2>Volume Tracker</h2>
+  <h4>{{volume}}</h4>
+  <button @click="volume--">-</button>
+  <button @click="volume++">+</button>
 </template>
 
 <script>
@@ -14,40 +11,17 @@ export default {
   name: 'App',
   data(){
     return {
-      firstName: 'Clark',
-      lastName: 'Kent',
-      prices: [1,2,3,4,5,6,7,8,9,10],
-      name:""
+      volume: 0
     }
   },
-  methods: {
-    getTotalAmount(){ 
-      console.log('getTotalAmount');
-      return this.prices.reduce((total, curr)=>(total+curr), 0)
-    },
-    changeFullName(){
-      this.fullName = 'Deepak Chaurasiya' // fullName will get passed to the set() function
-    }
-  },
-  computed: {
-    // fullName(){ //computed property "fullName" is readonly.
-    //   return `${this.firstName} ${this.lastName}`
-    // },
-    fullName: {
-      get(){
-        return `${this.firstName} ${this.lastName}`
-      },
-      set(value){
-        this.firstName = value.split(' ')[0];
-        this.lastName = value.split(' ')[1];
+  methods: {},
+  computed: {},
+  watch: {
+    volume(newValue, oldValue){
+      console.log(newValue, oldValue);
+      if(newValue>oldValue && newValue==7){
+        alert("High volume alert")
       }
-    },
-    totalAmount(){
-      console.log('totalAmount');
-      return this.prices.reduce((total, curr)=>(total+curr), 0)
-    },
-    highAmounts(){
-      return this.prices.filter(p=>p>5);
     }
   }
 }
