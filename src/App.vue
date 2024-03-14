80e6234163082c3df5ac556806984ab0ac9ad0f3
deepak252 <deepakkudc16102000@gmail.com>
2024-03-14 18:12:51 +0530

    Custom Events - child to parent

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,27 @@
 <template>
-  <!-- <Greet name="Bruce" heroName="Batman"/> -->
-  <!-- NOTE: Prefer using kebab-case for props in HTML and camelCase for the same in JS -->
-  <!-- <Greet name="Bruce" hero-name="Batman"/>
-  <Greet name="Clark" hero-name="Superman"/> 
-  <Greet :name="name" :hero-name="name2"/> -->
-  <!-- <Article :likes="8"/>
-  <Article title="Title 1" :likes="12"/>
-  <Article id="article-2" :title="'Title 2'" content="Lorem Ipsum" :likes="12"/> -->
-  <h2>App Username - {{name}}</h2>
-  <ComponentA />
+  <button @click="showPopup=!showPopup">Toggle Popup</button>
+  <!-- <Popup v-show="showPopup" @close="showPopup=false"/> -->
+  <Popup v-show="showPopup" @close="closePopup"/>
 </template>
-
 <script>
-// import Greet from'./components/Greet.vue'
-// import Article from'./components/Article.vue'
-import ComponentA from'./components/ComponentA.vue'
+import Popup from'./components/Popup.vue'
 
 export default {
   name: 'App',
   components: {
-    // Greet,
-    // Article,
-    ComponentA
+    Popup
   },
   data(){
     return {
-      name: 'Deepak',
-      name2: 'D.C.'
+      showPopup: false
     }
   },
-  // provide: {
-  //   username: 'Deepak'
-  // }
-  provide(){
-    return {
-      username: this.name
+  methods: {
+    closePopup(payload){
+      console.log({payload});
+      this.showPopup=false
     }
   }
-
 }
 </script>
 
---
+++ src/components/Popup.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>This is a Popup</h2>
+        <button @click="$emit('close', 'Popup Closed')">Close Popup</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Popup',
+    emits: ['close']
+}
+</script>
+
+<style>
+
+</style>
