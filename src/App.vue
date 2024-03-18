d0fe921a901d0fc35c0d5dc494f5217ce380b433
deepak252 <deepakkudc16102000@gmail.com>
2024-03-18 18:21:41 +0530

    Replacing Data with ref()

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
-    
+        <Data />
     </div>
 </template>
 
 <script>
+import Data from './components/Data.vue'
 
 export default {
     name: 'App',
     components: {
+        Data
     }
 }
 </script>
---
+++ src/components/Data.vue
@@ -0,0 +1,32 @@
+<template>
+    <h4> Options - {{o_firstName}} </h4>
+    <h4> Composition - {{c_firstName}} </h4> <!-- Deepak -->
+    <h4> Composition - {{greet}} </h4> <!-- Hello Daniel -->
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    name: 'Data',
+    data(){
+        return {
+            o_firstName: 'Deepak' // Options API
+        }
+    },
+    setup(){
+        const c_firstName = ref('Daniel'); // Composition API
+        console.log(c_firstName.value); // Daniel
+        const greet = `Hello ${c_firstName.value}`;
+        c_firstName.value = 'Deepak';
+        return {
+            c_firstName,
+            greet
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
